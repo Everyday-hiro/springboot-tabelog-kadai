15a0514c63c75259a5671d70e5a9f9fd9ce0e0d2
Everyday-hiro <hiromu00815@gmail.com>
2024-07-30 22:47:38 +0900

    認可やログインURLの各種設定をしました。

--- taberogu/src/main/java/com/example/taberogu/security/WebSecurityConfig.java
+++ taberogu/src/main/java/com/example/taberogu/security/WebSecurityConfig.java
@@ -1,5 +1,16 @@
 package com.example.taberogu.security;
 
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
+	@Bean
+	public SecurityFillterChain securityFilterChain(HttpSecurity http) throws Exception{
+		http
+		.authorizeHttpRequests((requests) -> requests
+		.requestMatchers("/css/**", "/image/**", "/storage/**", "/").permitAll()
+		.requestMatchers("/admin/**").
+		)
+	}
 }
