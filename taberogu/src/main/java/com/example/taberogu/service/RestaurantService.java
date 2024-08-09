f3421f5b7770602aaab16b2b90babc14c2d60bae
モリヒロム <pc.hiromu00815@gmail.com>
2024-08-09 17:06:40 +0900

    画像表示機能作成完了

--- taberogu/src/main/java/com/example/taberogu/service/RestaurantService.java
+++ taberogu/src/main/java/com/example/taberogu/service/RestaurantService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.taberogu.entity.Restaurant;
+import com.example.taberogu.form.RestaurantEditForm;
 import com.example.taberogu.form.RestaurantRegisterForm;
 import com.example.taberogu.repository.RestaurantRepository;
 
@@ -43,6 +44,32 @@ public class RestaurantService {
 		restaurant.setAddress(restaurantRegisterForm.getAddress());
 		restaurant.setPostalCode(restaurantRegisterForm.getPostalCode());
 		restaurant.setClosingDay(restaurantRegisterForm.getClosingDay());
+
+		restaurantRepository.save(restaurant);
+
+	}
+
+	@Transactional
+	public void update(RestaurantEditForm restaurantEditForm) {
+		Restaurant restaurant = restaurantRepository.getReferenceById(restaurantEditForm.getId());
+		MultipartFile imageFile = restaurantEditForm.getImageFile();
+
+		if (!imageFile.isEmpty()) {
+			String image = imageFile.getOriginalFilename();
+			String hashedImageName = generateNewFileName(image);
+			Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
+			copyImageFile(imageFile, filePath);
+			restaurant.setImage(hashedImageName);
+		}
+		restaurant.setName(restaurantEditForm.getName());
+		restaurant.setCategory(restaurantEditForm.getCategory());
+		restaurant.setRestaurantDescription(restaurantEditForm.getRestaurantDescription());
+		restaurant.setPhoneNumber(restaurantEditForm.getPhoneNumber());
+		restaurant.setOpenTime(restaurantEditForm.getOpenTime());
+		restaurant.setPrice(restaurantEditForm.getPrice());
+		restaurant.setAddress(restaurantEditForm.getAddress());
+		restaurant.setPostalCode(restaurantEditForm.getPostalCode());
+		restaurant.setClosingDay(restaurantEditForm.getClosingDay());
 
 		restaurantRepository.save(restaurant);
 
