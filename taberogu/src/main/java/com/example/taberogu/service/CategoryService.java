c27b1e00e8196a6f0808fb30b0faf3a50914331a
モリヒロム <pc.hiromu00815@gmail.com>
2024-08-10 17:06:24 +0900

    カテゴリ登録機能の完成

---
+++ taberogu/src/main/java/com/example/taberogu/service/CategoryService.java
@@ -0,0 +1,25 @@
+package com.example.taberogu.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.taberogu.entity.Category;
+import com.example.taberogu.form.CategoryRegisterForm;
+import com.example.taberogu.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+	private final CategoryRepository categoryRepository;
+
+	public CategoryService(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+
+	@Transactional
+	public void create(CategoryRegisterForm categoryRegisterForm) {
+		Category category = new Category();
+
+		category.setName(categoryRegisterForm.getName());
+		categoryRepository.save(category);
+	}
+}
