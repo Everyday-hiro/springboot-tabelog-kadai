ddb91a44a4994309e4ff4caf5fa2b86aa510cf07
モリヒロム <pc.hiromu00815@gmail.com>
2024-08-19 12:50:37 +0900

    トップページのカテゴリをDBをもとに表示させました

--- taberogu/src/main/java/com/example/taberogu/controller/TopPageController.java
+++ taberogu/src/main/java/com/example/taberogu/controller/TopPageController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.taberogu.entity.Category;
 import com.example.taberogu.entity.Favorite;
 import com.example.taberogu.entity.Restaurant;
+import com.example.taberogu.repository.CategoryRepository;
 import com.example.taberogu.repository.FavoriteRepository;
 import com.example.taberogu.repository.RestaurantRepository;
 
@@ -15,18 +17,23 @@ import com.example.taberogu.repository.RestaurantRepository;
 public class TopPageController {
 	private final RestaurantRepository restaurantRepository;
 	private final FavoriteRepository favoriteRepository;
+	private final CategoryRepository categoryRepository;
 
-	public TopPageController(RestaurantRepository restaurantRepository, FavoriteRepository favoriteRepository) {
+	public TopPageController(RestaurantRepository restaurantRepository, FavoriteRepository favoriteRepository,
+			CategoryRepository categoryRepository) {
 		this.restaurantRepository = restaurantRepository;
 		this.favoriteRepository = favoriteRepository;
+		this.categoryRepository = categoryRepository;
 	}
 
 	@GetMapping("/")
 	public String index(Model model) {
 		List<Restaurant> newRestaurant = restaurantRepository.findTop3ByOrderByCreatedAtDesc();
 		List<Favorite> newFavorite = favoriteRepository.findTop3ByOrderByCreatedAtDesc();
+		List<Category> category = categoryRepository.findAll();
 		model.addAttribute("newFavorite", newFavorite);
 		model.addAttribute("newRestaurant", newRestaurant);
+		model.addAttribute("category", category);
 		return "index";
 	}
 }
