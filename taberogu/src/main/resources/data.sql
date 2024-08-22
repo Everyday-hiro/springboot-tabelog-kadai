9fbeb98735ef5814f1bc5a0efdc5bb91f7004475
モリヒロム <pc.hiromu00815@gmail.com>
2024-08-22 14:21:50 +0900

    クレジットカードのDB作成

--- taberogu/src/main/resources/data.sql
+++ taberogu/src/main/resources/data.sql
@@ -1,3 +1,14 @@
+INSERT IGNORE INTO category (id, name) VALUES(1, "カフェ");
+INSERT IGNORE INTO category (id, name) VALUES(2, "焼肉");
+INSERT IGNORE INTO category (id, name) VALUES(3, "寿司");
+INSERT IGNORE INTO category (id, name) VALUES(4, "中華");
+INSERT IGNORE INTO category (id, name) VALUES(5, "丼物");
+INSERT IGNORE INTO category (id, name) VALUES(6, "和食");
+
+INSERT IGNORE INTO role(id, name) VALUES (1, 'ROLE_FREE');
+INSERT IGNORE INTO role(id, name) VALUES (2, 'ROLE_PAID');
+INSERT IGNORE INTO role(id, name) VALUES (3, 'ROLE_ADMIN');
+
 INSERT IGNORE INTO restaurant (id, category_id, name, image, restaurant_description, phone_number, open_time, price, address, postal_code, closing_day) VALUES (1, 1, 'SAMURAI COFFEE', 'house01.jpg', '最寄駅から10分の場所にあり割といい立地である。', '052-334-5347', '9:00~23:00', '1000円~2000円', '愛知県名古屋市天白区大岡2-3290' , '457-6609', '木曜日');
 INSERT IGNORE INTO restaurant (id, category_id, name, image, restaurant_description, phone_number, open_time, price, address, postal_code, closing_day) VALUES (2, 1, '侍の喫茶', 'house02.jpg', '最寄駅から10分の場所にあり割といい立地である。', '052-334-5347', '9:00~22:00', '1000円~2000円', '愛知県名古屋市東区山岳2-3290' , '457-6609', '金曜日');
 INSERT IGNORE INTO restaurant (id, category_id, name, image, restaurant_description, phone_number, open_time, price, address, postal_code, closing_day) VALUES (3, 2, 'サムライ焼き', 'house03.jpg', '最寄駅から10分の場所にあり割といい立地である。', '052-334-5347', '17:00~23:00', '2000円~3000円', '愛知県名古屋市西区豊本2-3290' , '457-6609', '土曜日');
@@ -9,10 +20,6 @@ INSERT IGNORE INTO restaurant (id, category_id, name, image, restaurant_descript
 INSERT IGNORE INTO restaurant (id, category_id, name, image, restaurant_description, phone_number, open_time, price, address, postal_code, closing_day) VALUES (9, 5, 'SAMURAI屋', 'house09.jpg', '最寄駅から10分の場所にあり割といい立地である。', '052-334-5347', '10:00~23:00', '1000円~2000円', '愛知県名古屋市中区牡丹2-3290' , '457-6609', '金曜日');
 INSERT IGNORE INTO restaurant (id, category_id, name, image, restaurant_description, phone_number, open_time, price, address, postal_code, closing_day) VALUES (10, 5, '侍丼', 'house10.jpg', '最寄駅から10分の場所にあり割といい立地である。', '052-334-5347', '9:00~22:00', '1000円~2000円', '愛知県名古屋市関西区都響2-3290' , '457-6609', '土曜日');
 INSERT IGNORE INTO restaurant (id, category_id, name, image, restaurant_description, phone_number, open_time, price, address, postal_code, closing_day) VALUES (11, 6, '和食侍', 'house01.jpg', '最寄駅から10分の場所にあり割といい立地である。', '052-334-5347', '12:00~20:00', '2000円~4000円', '愛知県名古屋市関東区加賀和那2-3290' , '457-6609', '日曜日');
-
-INSERT IGNORE INTO role(id, name) VALUES (1, 'ROLE_FREE');
-INSERT IGNORE INTO role(id, name) VALUES (2, 'ROLE_PAID');
-INSERT IGNORE INTO role(id, name) VALUES (3, 'ROLE_ADMIN');
 
 INSERT IGNORE INTO user (id, name, furigana, postal_code, address, phone_number, email, password, role_id, enabled) VALUES (1, '侍 太郎', 'サムライ タロウ', '101-0022', '東京都千代田区神田練塀町300番地', '090-1234-5678', 'taro.samurai@example.com', '$2a$10$2JNjTwZBwo7fprL2X4sv.OEKqxnVtsVQvuXDkI8xVGix.U3W5B7CO', 1, true);
 INSERT IGNORE INTO user (id, name, furigana, postal_code, address, phone_number, email, password, role_id, enabled) VALUES (2, '侍 花子', 'サムライ ハナコ', '101-0022', '東京都千代田区神田練塀町300番地', '090-1234-5678', 'hanako.samurai@example.com', '$2a$10$2JNjTwZBwo7fprL2X4sv.OEKqxnVtsVQvuXDkI8xVGix.U3W5B7CO', 2, true);
@@ -40,13 +47,6 @@ INSERT IGNORE INTO favorite (id, user_id, restaurant_id) VALUES (3, 2, 5);
 INSERT IGNORE INTO favorite (id, user_id, restaurant_id) VALUES (4, 2, 7);
 INSERT IGNORE INTO favorite (id, user_id, restaurant_id) VALUES (5, 2, 9);
 INSERT IGNORE INTO favorite (id, user_id, restaurant_id) VALUES (6, 2, 11);
-
-INSERT IGNORE INTO category (id, name) VALUES(1, "カフェ");
-INSERT IGNORE INTO category (id, name) VALUES(2, "焼肉");
-INSERT IGNORE INTO category (id, name) VALUES(3, "寿司");
-INSERT IGNORE INTO category (id, name) VALUES(4, "中華");
-INSERT IGNORE INTO category (id, name) VALUES(5, "丼物");
-INSERT IGNORE INTO category (id, name) VALUES(6, "和食");
 
 INSERT IGNORE INTO reservation (id, restaurant_id, user_id, checkin_date, number_of_people, amount) VALUES (1, 1, 2, '2023-04-01　13:00', 2, 2000);
 INSERT IGNORE INTO reservation (id, restaurant_id, user_id, checkin_date, number_of_people, amount) VALUES (2, 2, 2, '2023-04-01 17:30', 3, 3000);
